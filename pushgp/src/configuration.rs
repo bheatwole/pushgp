e19346357bc01a36f21f399112bfd1d5805e97ef
Benjamin Heatwole <benjaminheatwole@cloudfitsoftware.com>
2021-08-14 13:19:55 +0000

    Moved setup of allowed_instructions out of 'new' to make testing easier

--- pushgp/src/configuration.rs
+++ pushgp/src/configuration.rs
@@ -8,32 +8,9 @@ pub struct Configuration {
 
 impl Configuration {
     pub fn new() -> Configuration {
-        let mut config = Configuration {
+        Configuration {
             allowed_instructions: FnvHashSet::default(),
-        };
-        config.allowed_instructions.insert(Instruction::BoolAnd);
-        config.allowed_instructions.insert(Instruction::BoolDefine);
-        config.allowed_instructions.insert(Instruction::BoolDup);
-        config.allowed_instructions.insert(Instruction::BoolEqual);
-        config.allowed_instructions.insert(Instruction::BoolFlush);
-        config
-            .allowed_instructions
-            .insert(Instruction::BoolFromFloat);
-        config.allowed_instructions.insert(Instruction::BoolFromInt);
-        config.allowed_instructions.insert(Instruction::BoolNot);
-        config.allowed_instructions.insert(Instruction::BoolOr);
-        config.allowed_instructions.insert(Instruction::BoolPop);
-        config.allowed_instructions.insert(Instruction::BoolRand);
-        config.allowed_instructions.insert(Instruction::BoolRot);
-        config.allowed_instructions.insert(Instruction::BoolShove);
-        config
-            .allowed_instructions
-            .insert(Instruction::BoolStackDepth);
-        config.allowed_instructions.insert(Instruction::BoolSwap);
-        config.allowed_instructions.insert(Instruction::BoolYank);
-        config.allowed_instructions.insert(Instruction::BoolYankDup);
-
-        config
+        }
     }
 
     pub fn disallow_type(&mut self, disallowed: InstructionType) {
@@ -55,5 +32,9 @@ impl Configuration {
 
     pub fn disallow_instruction(&mut self, disallow: Instruction) {
         self.allowed_instructions.remove(&disallow);
+    }
+
+    pub fn allow_instruction(&mut self, allow: Instruction) {
+        self.allowed_instructions.insert(allow);
     }
 }

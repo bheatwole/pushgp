e928cbc51ce64e3d3876a4c73521c756690ac295
Benjamin Heatwole <benjaminheatwole@cloudfitsoftware.com>
2022-06-05 07:06:24 -0400

    Cleanup of names

--- pushgp/src/instruction_type.rs
+++
@@ -1,9 +0,0 @@
-/// Represents which stack an instruction may interact with.
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
-pub enum InstructionType {
-    Bool,
-    Code,
-    Float,
-    Int,
-    Name,
-}

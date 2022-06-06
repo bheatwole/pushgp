3966014cd748295ce5aa9262615bdba768b005c6
Benjamin Heatwole <benjaminheatwole@cloudfitsoftware.com>
2022-06-06 07:36:56 -0400

    Updated Island to remove Context trait

--- pushgp/src/island.rs
+++ pushgp/src/island.rs
@@ -1,9 +1,9 @@
-use crate::Individual;
+use crate::{Context, Individual};
 
-pub trait Island<C, S> {
+pub trait Island<State: std::fmt::Debug + Clone> {
     fn name() -> &'static str;
     fn pre_generation_run(&mut self);
     fn post_generation_run(&mut self);
-    fn run_individual(&mut self, context: &C, individual: &mut Individual<S>);
-    fn sort_individuals(&self, a: &Individual<S>, b: &Individual<S>) -> std::cmp::Ordering;
+    fn run_individual(&mut self, context: &mut Context<State>, individual: &mut Individual<State>);
+    fn sort_individuals(&self, a: &Individual<State>, b: &Individual<State>) -> std::cmp::Ordering;
 }

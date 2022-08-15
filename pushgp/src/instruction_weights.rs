65d4032bebac4a97bde12a39231c614538db0760
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-08-15 07:52:36 -0400

    Added method to find if an instruction exists

--- pushgp/src/instruction_weights.rs
+++ pushgp/src/instruction_weights.rs
@@ -2,6 +2,7 @@ use crate::{Code, Configuration, StaticInstruction, VirtualMachine};
 
 pub type GenerateFn<Vm> = fn(vm: &mut Vm) -> Code<Vm>;
 
+/// This struct tracks the weights associated with each instruction, and allows quickly picking a random instruction.
 #[derive(PartialEq)]
 pub struct InstructionWeights<Vm: VirtualMachine> {
     instructions: Vec<InstructionEntry<Vm>>,
@@ -13,15 +14,33 @@ impl<Vm: VirtualMachine> InstructionWeights<Vm> {
         InstructionWeights { instructions: vec![], sum_of_weights: 0 }
     }
 
+    /// Adds the specified instruction to the weight list. The instruction must meet all the compile-time traits of the
+    /// Vm or this will fail to compile. (i.e. if the Vm does not implement VirtualMachineMustHaveName, adding an
+    /// instruction that uses the Name stack will fail to compile.)
     pub fn add_instruction<C: StaticInstruction<Vm>>(&mut self, weight: u8) {
         self.sum_of_weights += weight as usize;
         self.instructions.push(InstructionEntry {
             name: C::static_name(),
+            weight: weight,
             combined_weight: self.sum_of_weights,
             generate: C::random_value,
         });
     }
 
+    /// Returns the weight of the instruction with the specified name, or None
+    pub fn weight_of_named_instruction(&self, name: &'static str) -> Option<u8> {
+        if let Some(entry) = self.instructions.iter().find(|entry| entry.name == name) {
+            Some(entry.weight)
+        } else {
+            None
+        }
+    }
+
+    /// One of the possible genetic algorithms is to adjust the weights of instructions of new random indiviudals such
+    /// that we get the best possible outcome from random code by limiting instructions that don't help and increasing
+    /// the liklihood of instructions that do.
+    /// 
+    /// This function resets the weights of all instructions based on a new configuration.
     pub fn reset_weights_from_configuration(&mut self, config: &Configuration) {
         let mut next_sum_of_weights = 0;
         for entry in self.instructions.iter_mut() {
@@ -31,10 +50,14 @@ impl<Vm: VirtualMachine> InstructionWeights<Vm> {
         self.sum_of_weights = next_sum_of_weights;
     }
 
+    /// Returns the total sum of all weights so that other code can include a range of all instruction weights in a
+    /// grand total random number choice.
     pub fn get_sum_of_weights(&self) -> usize {
         self.sum_of_weights
     }
 
+    /// Picks a random instruction from all the instructions that are defined, and returns a function that will create
+    /// a new piece of Code when called.
     pub fn pick_random_instruction_generator<R: rand::Rng>(&self, rng: &mut R) -> GenerateFn<Vm> {
         let pick = rng.gen_range(1..=self.sum_of_weights);
         let index = self.instructions.partition_point(|entry| entry.combined_weight < pick);
@@ -42,7 +65,8 @@ impl<Vm: VirtualMachine> InstructionWeights<Vm> {
     }
 }
 
-// The default implementation is too chatty for this object, which appears in the test output
+// The default implementation is too chatty for this object, which appears in the test output and obfuscates the actual
+// test results.
 impl<Vm: VirtualMachine> std::fmt::Debug for InstructionWeights<Vm> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "InstructionWeights: weight of {} for {} instructions", self.sum_of_weights, self.instructions.len())
@@ -51,6 +75,9 @@ impl<Vm: VirtualMachine> std::fmt::Debug for InstructionWeights<Vm> {
 
 struct InstructionEntry<Vm> {
     pub name: &'static str,
+
+    // The weight assigned to this instruction
+    pub weight: u8,
 
     // The weight of this entry combined with the sum of weight of every entry sorted before it.
     pub combined_weight: usize,

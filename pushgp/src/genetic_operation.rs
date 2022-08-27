2caa4526708c5ffeacff1609653e6cafa6d5dd3d
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-08-27 07:44:39 -0400

    Added comments as to why ExtractFunction can't be implemented yet

--- pushgp/src/genetic_operation.rs
+++ pushgp/src/genetic_operation.rs
@@ -4,6 +4,10 @@ pub enum GeneticOperation {
 
     // TODO: ExtractFunction: a random point in the single parent's code is replaced with a new random name and the name
     // defined as the code that was at that point
+    // BLOCKER: This feature would require inserting a NameLiteralValue into the code, which, in turn, would require
+    // that the VM implements VirtualMachineMustHaveName. The only two effective ways around this are to require every
+    // VM to have a name stack (which introduces other difficulties) or to wait until the `specialization` RFC becomes
+    // stable (or at least close to stable) https://github.com/rust-lang/rust/issues/31844
 
     // TODO: MutationIncludingFunctions: when selecting the mutation points, all of a parent's defined_names are counted
     // as well and the mutation could occur there

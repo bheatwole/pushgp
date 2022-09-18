2b2bf1aca96393442750615d52ae72b6b9d16b82
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-09-18 04:57:55 -0400

    Implemented new

--- pushgp-weights/src/vm.rs
+++ pushgp-weights/src/vm.rs
@@ -1,6 +1,9 @@
+use fnv::FnvHashMap;
 use pushgp::*;
 
-use crate::{InstructionName, Weight, VirtualMachineMustHaveWeight, VirtualMachineMustHaveInstructionName};
+use crate::{
+    InstructionName, VirtualMachineMustHaveInstructionName, VirtualMachineMustHaveWeight, Weight,
+};
 
 /// InstructionWeightVirtualMachine is used to determine the optimal instruction weights for another VirtualMachine. It
 /// does this by executing the initial run of random individuals on another world and using the genetic algorithm with
@@ -8,15 +11,43 @@ use crate::{InstructionName, Weight, VirtualMachineMustHaveWeight, VirtualMachin
 ///
 /// To effectively use InstructionWeightVirtualMachine the Islands of the target World must all implement the
 /// `score_instruction` function in the `IslandCallbacks` trait.
-#[derive(Debug, PartialEq)]
-pub struct InstructionWeightVirtualMachine<
-    TargetRunResult: RunResult,
-    TargetVm: VirtualMachine,
-> {
+#[derive(Clone, Debug, PartialEq)]
+pub struct InstructionWeightVirtualMachine<TargetRunResult: RunResult, TargetVm: VirtualMachine> {
     engine: VirtualMachineEngine<InstructionWeightVirtualMachine<TargetRunResult, TargetVm>>,
     instruction_name_stack: Stack<InstructionName>,
     weight_stack: Stack<Weight>,
     target_world: World<TargetRunResult, TargetVm>,
+}
+
+impl<TargetRunResult: RunResult, TargetVm: VirtualMachine>
+    InstructionWeightVirtualMachine<TargetRunResult, TargetVm>
+{
+    pub fn new(
+        world: &World<TargetRunResult, TargetVm>,
+    ) -> InstructionWeightVirtualMachine<TargetRunResult, TargetVm> {
+        let mut vm = InstructionWeightVirtualMachine {
+            engine: VirtualMachineEngine::new(
+                None,
+                Configuration::new(65536, 100, 99, 1, 0, FnvHashMap::default()),
+            ),
+            instruction_name_stack: Stack::new(),
+            weight_stack: Stack::new(),
+            target_world: world.clone(),
+        };
+
+        vm.add_instructions();
+
+        vm
+    }
+
+    fn add_instructions(&mut self) {
+        self.engine_mut()
+            .add_instruction::<crate::instruction_name::InstructionNameLiteralValue>();
+        self.engine_mut()
+            .add_instruction::<crate::set_instruction_weight::ExecSetInstructionWeight>();
+        self.engine_mut()
+            .add_instruction::<crate::weight::WeightLiteralValue>();
+    }
 }
 
 impl<TargetRunResult: RunResult, TargetVm: VirtualMachine> VirtualMachine
@@ -51,8 +82,9 @@ impl<TargetRunResult: RunResult, TargetVm: VirtualMachine>
 }
 
 impl<TargetRunResult: RunResult, TargetVm: VirtualMachine>
-    VirtualMachineMustHaveInstructionName<InstructionWeightVirtualMachine<TargetRunResult, TargetVm>>
-    for InstructionWeightVirtualMachine<TargetRunResult, TargetVm>
+    VirtualMachineMustHaveInstructionName<
+        InstructionWeightVirtualMachine<TargetRunResult, TargetVm>,
+    > for InstructionWeightVirtualMachine<TargetRunResult, TargetVm>
 {
     fn instruction_name(&mut self) -> &mut Stack<InstructionName> {
         &mut self.instruction_name_stack
@@ -68,8 +100,8 @@ impl<TargetRunResult: RunResult, TargetVm: VirtualMachine>
     }
 }
 
-impl<TargetRunResult: RunResult, TargetVm: VirtualMachine>
-    DoesVirtualMachineHaveName for InstructionWeightVirtualMachine<TargetRunResult, TargetVm>
+impl<TargetRunResult: RunResult, TargetVm: VirtualMachine> DoesVirtualMachineHaveName
+    for InstructionWeightVirtualMachine<TargetRunResult, TargetVm>
 {
     const HAS_NAME: bool = false;
 }

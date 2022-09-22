4f191bee2f253c6b74e4f212d8a73c4d46d5ed35
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-09-22 07:00:18 -0400

    Added ExecutionError type

---
+++ pushgp/src/execution_error.rs
@@ -0,0 +1,14 @@
+/// Used to determine how a program's code performed when run on the virtual machine
+pub enum ExecutionError {
+    /// The instruction attempted to perform an illegal operation (such as divide by zero). All stack values used by the
+    /// instruction were removed.
+    IllegalOperation,
+
+    /// The instruction needed a certain number and kind of stack values and there were not enough. All stacks left as
+    /// they were
+    InsufficientInputs,
+
+    /// The program used more memory than allowed. This could be trying to push to a stack that is full, or trying to
+    /// allocate on a heap that is full.
+    OutOfMemory,
+}
